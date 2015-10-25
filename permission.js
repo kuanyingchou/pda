c202c77026b535aa5d14ed81c690623f8d13b8e1
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-10-25 02:09:09 -0400

    fix permission

---
+++ permission.js
@@ -0,0 +1,9 @@
+navigator.webkitGetUserMedia({
+    audio: true,
+}, function(stream) {
+    stream.stop();
+    // Now you know that you have audio permission. Do whatever you want...
+}, function() {
+    // Aw. No permission (or no microphone available).
+    console.log('no permission!');
+});
