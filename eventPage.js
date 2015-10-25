42380eb40489f456f68eb1fccc758c70ecb07c4e
KUAN-YING CHOU <kuanyingchou@gmail.com>
2015-10-25 02:08:35 -0400

    rm html; add commands

--- eventPage.js
+++ eventPage.js
@@ -1,9 +1,11 @@
-
-
 var final_transcript = '';
 var interim_transcript = '';
 var recognizing = false;
 var ignore_onend;
+var magic_word = 'educate';
+
+window.open(chrome.extension.getURL("permission.html"));
+
 
 if (!('webkitSpeechRecognition' in window)) {
   upgrade();
@@ -15,16 +17,13 @@ if (!('webkitSpeechRecognition' in window)) {
   recognition.onstart = function() {
     recognizing = true;
     showInfo('info_speak_now');
-    start_img.src = 'mic-animate.gif';
   };
   recognition.onerror = function(event) {
     if (event.error == 'no-speech') {
-      start_img.src = 'mic.gif';
       showInfo('info_no_speech');
       ignore_onend = true;
     }
     if (event.error == 'audio-capture') {
-      start_img.src = 'mic.gif';
       showInfo('info_no_microphone');
       ignore_onend = true;
     }
@@ -38,25 +37,19 @@ if (!('webkitSpeechRecognition' in window)) {
     if (ignore_onend) {
       return;
     }
-    start_img.src = 'mic.gif';
     if (!final_transcript) {
       showInfo('info_start');
       return;
     }
     showInfo('');
-    if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-      var range = document.createRange();
-      range.selectNode(document.getElementById('final_span'));
-      window.getSelection().addRange(range);
-    }
   };
   recognition.onresult = function(event) {
+    //console.log('got result');
     interim_transcript = '';
     var trans = '';
     for (var i = event.resultIndex; i < event.results.length; ++i) {
       var t = event.results[i][0].transcript;
-      //console.log(i+': '+t);
+      console.log(i+': '+t);
       if (event.results[i].isFinal) {
         trans += t;
       } else {
@@ -66,11 +59,7 @@ if (!('webkitSpeechRecognition' in window)) {
     addFinalTranscript(trans);
 
     final_transcript = capitalize(final_transcript);
-    final_span.innerHTML = linebreak(final_transcript);
-    interim_span.innerHTML = linebreak(interim_transcript);
-    if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
-    }
+    
   };
 }
 function upgrade() {
@@ -96,22 +85,11 @@ function startButton(event) {
   recognition.lang = 'en-US';
   recognition.start();
   ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  start_img.src = 'mic-slash.gif';
   showInfo('info_allow');
-  showButtons('none');
 }
 function showInfo(s) {
   if (s) {
-    for (var child = info.firstChild; child; child = child.nextSibling) {
-      if (child.style) {
-        child.style.display = child.id == s ? 'inline' : 'none';
-      }
-    }
-    info.style.visibility = 'visible';
-  } else {
-    info.style.visibility = 'hidden';
+    console.log(s);
   }
 }
 var current_style;
@@ -140,15 +118,14 @@ function addFinalTranscript(t) {
   var words = t.split(' ');
   for(var i = 0; i<words.length; i++) {
     //console.log('checking '+words[i]);
-    if(words[i].toLowerCase() === 'google') {
+    if(words[i].toLowerCase() === magic_word) {
       
       words = words.slice(i);
       //words.push(';\n');
       final_transcript += words.join(' ');
       handle({
         target: words[0],
-        command: words[1],
-        parameters: words.slice(2)
+        command: words.slice(1)
       });
       //console.log("> "+final_transcript);
       break;
@@ -163,17 +140,46 @@ function handle(request) {
 
   requests.push(request);
 
-  switch(request.command) {
-    case "search":
+  switch(true) {
+    case /^search/.test(request.command):
       console.log(request);
+      
+      var msg = new SpeechSynthesisUtterance('searching '+ 
+          request.command.slice(1).join(' '));
+      msg.lang = 'en-US';
+      window.speechSynthesis.speak(msg);
+
+      window.open('https://www.google.com/#q='+
+          request.command.slice(1).join('+'));
       break;
-    case "open":
+    case /^open/.test(request.command):
       console.log(request);
+
+      break;
+    case /^press/.test(request.command):
+      var page_up = 33;
+      var page_down = 34;
+      if(request.command.slice(1).join(' ') === 'page up') {
+        console.log("press page up");
+        press(page_up);
+      } else if(request.command.slice(1).join(' ' === 'page down')) {
+        console.log("press page down");
+        press(page_down);
+      }
+      
       break;
       //...
     default:
       break;
   }
+}
+
+function press(keycode) {
+  $(function() {
+      var e = $.Event('keypress');
+      e.which = keycode; // Character 'A'
+      $('document').trigger(e);
+  });
 }
 
 startButton(null);
